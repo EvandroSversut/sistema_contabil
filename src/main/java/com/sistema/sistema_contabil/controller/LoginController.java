c7afdf65f0a390d12fbe2c8ded340d218769ac50
Evandro <svecontabilidade@gmail.com>
2025-05-15 07:00:27 -0300

    login controller - so falta fechar uma chave do metodo

--- src/main/java/com/sistema/sistema_contabil/controller/LoginController.java
+++ src/main/java/com/sistema/sistema_contabil/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,12 +71,17 @@ public class LoginController {
     public String criarUsuario(@RequestBody Usuario usuario) {
         return loginService.cadastrarUsuario(usuario);
     }
+
+
+
+// Exemplo no Controller
+@GetMapping("/api/teste-protegido")
+public ResponseEntity<String> testeProtegido() {
+    return ResponseEntity.ok("Token JWT válido. Acesso permitido!");
 }
 
 
-
-
-
+}
 
 
 
